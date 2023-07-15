7e6952f23833b02b1232d27aea7a57006f5fbef0
SumanthSMayakar <sumanthmayakar@gmail.com>
2023-07-15 14:51:44 +0530

    roll-dice completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,20 @@
 
 let score0 = document.getElementById('score--0');
 let score1 = document.getElementById('score--1');
-let dice = document.querySelector('.dice')
-let rollDice = document.querySelector('.btn--roll')
-let current1 = document.getElementById('current--0')
+let dice = document.querySelector('.dice');
+let rollDice = document.querySelector('.btn--roll');
+let current1 = document.getElementById('current--0');
+let player0 = document.querySelector('.player--0');
+let player1 = document.querySelector('.player--1');
 
 // initialize the value to zero
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add('hidden');
 
+let scores = [0,0];
 let current = 0;
+let activePlayer = 0;
 
 // Implement functionality for roll dice button 
 rollDice.addEventListener('click', function(){
@@ -26,7 +30,14 @@ rollDice.addEventListener('click', function(){
     // 3. if the random number is not 1 then add it to active player current score 
     if(diceNumber !=1){
         current += diceNumber;
-        current1.textContent = current;
+        document.getElementById(`current--${activePlayer}`).textContent = current;
+        // current1.textContent = current;
+    }else{
+        current = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = current;
+        activePlayer = activePlayer === 0? 1: 0;
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
     }
     // 4. if random number is 1 then reset current score to zero and change the active player 
 })
